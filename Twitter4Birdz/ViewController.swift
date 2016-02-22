513079b4f31df74d81d310133c4301fab8ddfd4c
Matt Del Signore <mattdelsig@me.com>
2016-02-21 22:58:32 -0500

    Initial Commit

---
+++ Twitter4Birdz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter4Birdz
+//
+//  Created by Matt on 2/21/16.
+//  Copyright © 2016 Matt Del Signore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
